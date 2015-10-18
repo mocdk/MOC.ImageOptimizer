198678b25f20733fd76bd5b56fc1928c34090e82
Aske Ertmann <aertmann@gmail.com>
2015-10-18 13:58:06 +0200

    [TASK] Only process media thumbnail image files
    Changes the aspect from targeting all published resource files to
    instead only target generated thumbnail images from the media package.
    
    This solves three important issues:
    
     - Only generated thumbnails will affected instead of all image files
     - Thumbnails will only be processed once when generated
     - Original files won't be affected since a thumbnail is always created
    
    It's easy to regenerate thumbnail files by simply running the command::
    
      ./flow media:clearthumbnails
    
    This change removes compatibility with Flow in general and instead relies
    on the media package in a version requiring Flow 3.x. Which means it's no
    longer compabitle with Neos 1.2.

---
+++ Classes/Aspects/ThumbnailAspect.php
@@ -0,0 +1,127 @@
+<?php
+namespace MOC\ImageOptimizer\Aspects;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Aop\JoinPointInterface;
+use TYPO3\Flow\Log\SystemLoggerInterface;
+use TYPO3\Flow\Package\PackageManagerInterface;
+use TYPO3\Flow\Resource\ResourceManager;
+
+/**
+ * @Flow\Scope("singleton")
+ * @Flow\Aspect
+ */
+class ThumbnailAspect {
+
+	/**
+	 * @var SystemLoggerInterface
+	 * @Flow\Inject
+	 */
+	protected $systemLogger;
+
+	/**
+	 * @Flow\Inject
+	 * @var PackageManagerInterface
+	 */
+	protected $packageManager;
+
+	/**
+	 * @Flow\Inject
+	 * @var ResourceManager
+	 */
+	protected $resourceManager;
+
+	/**
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
+	 * @param array $settings
+	 * @return void
+	 */
+	public function injectSettings(array $settings) {
+		$this->settings = $settings;
+	}
+
+	/**
+	 * After a thumbnail has been refreshed the resource is optimized, meaning the
+	 * image is only optimized once when created.
+	 *
+	 * A new resource is generated for every thumbnail, meaning the original is
+	 * never touched.
+	 *
+	 * Only local file system target is supported to keep it from being blocking.
+	 * It would however be possible to create a local copy of the resource,
+	 * process it, import it and set that as the thumbnail resource.
+	 *
+	 * @Flow\AfterReturning("method(TYPO3\Media\Domain\Model\Thumbnail->refresh())")
+	 * @param \TYPO3\Flow\Aop\JoinPointInterface $joinPoint The current join point
+	 * @return void
+	 */
+	public function optimizeThumbnail(JoinPointInterface $joinPoint) {
+		/** @var \TYPO3\Media\Domain\Model\Thumbnail $thumbnail */
+		$thumbnail = $joinPoint->getProxy();
+		$thumbnailResource = $thumbnail->getResource();
+
+		// Easy workaround for getting local path since the resource API doesn't allow it due to support for non local file system targets
+		$pathAndFilename = realpath(FLOW_PATH_WEB . parse_url($this->resourceManager->getPublicPersistentResourceUri($thumbnailResource), PHP_URL_PATH));
+
+		$useGlobalBinary = $this->settings['useGlobalBinary'];
+		$binaryRootPath = 'Private/Library/node_modules/';
+		$file = escapeshellarg($pathAndFilename);
+		$imageType = $thumbnailResource->getMediaType();
+		switch ($imageType) {
+			case 'image/jpeg':
+				if ($this->settings['formats']['jpg']['enabled'] === FALSE) {
+					return;
+				}
+				$library = 'jpegtran';
+				$binaryPath = sprintf('%1$s-bin/vendor/%s', $library);
+				$arguments = sprintf('-copy none -optimize %s -outfile %s %s', $this->settings['formats']['jpg']['progressive'] === TRUE ? '-progressive' : '', $file, $file);
+				if ($this->settings['formats']['jpg']['useGlobalBinary'] === TRUE) {
+					$useGlobalBinary = TRUE;
+				}
+				break;
+			case 'image/png':
+				if ($this->settings['formats']['png']['enabled'] === FALSE) {
+					return;
+				}
+				$library = 'optipng';
+				$binaryPath = sprintf('%1$s-bin/vendor/%s', $library);
+				$arguments = sprintf('-o%u -strip all -out %s %s', $this->settings['formats']['png']['optimizationLevel'], $file, $file);
+				if ($this->settings['formats']['png']['useGlobalBinary'] === TRUE) {
+					$useGlobalBinary = TRUE;
+				}
+				break;
+			case 'image/gif':
+				if ($this->settings['formats']['gif']['enabled'] === FALSE) {
+					return;
+				}
+				$library = 'gifsicle';
+				$binaryPath = sprintf('%1$s/vendor/%1$s', $library);
+				$arguments = sprintf('--batch -O%u %s ', $this->settings['formats']['gif']['optimizationLevel'], $file);
+				if ($this->settings['formats']['gif']['useGlobalBinary'] === TRUE) {
+					$useGlobalBinary = TRUE;
+				}
+				break;
+			case 'image/svg+xml':
+				if ($this->settings['formats']['svg']['enabled'] === FALSE) {
+					return;
+				}
+				$library = 'svgo';
+				$binaryPath = sprintf('%1$s/bin/%1$s', $library);
+				$arguments = sprintf('%s %s', $this->settings['formats']['svg']['pretty'] === TRUE ? '--pretty' : '', $file);
+				if ($this->settings['formats']['svg']['useGlobalBinary'] === TRUE) {
+					$useGlobalBinary = TRUE;
+				}
+				break;
+		}
+		$binaryPath = $useGlobalBinary === TRUE ? $this->settings['globalBinaryPath'] . $library : $this->packageManager->getPackageOfObject($this)->getResourcesPath() . $binaryRootPath . $binaryPath;
+		$cmd = escapeshellcmd($binaryPath) . ' ' . $arguments;
+		$output = [];
+		exec($cmd, $output, $result);
+		$this->systemLogger->log($cmd . ' (' . ((int)$result === 0 ? 'OK' : 'Error: ' . $result) . ')', LOG_INFO, $output);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,20 +1,22 @@
 MOC.ImageOptimizer
 ==================
 
-TYPO3 Flow package that optimizes published images (jpg, png, gif, svg) for web presentation.
+Flow package that optimizes generated thumbnail images (jpg, png, gif, svg) for web presentation.
+
+Original files are never affected since copies are always created for thumbnails.
 
 Using jpegtran, optipng, gifsicle and svgo for the optimizations.
 
 Should work with Linux, FreeBSD, OSX, SunOS & Windows (only tested Linux & FreeBSD so far).
 
-Works with TYPO3 Flow 1.x, 2.x
+Compatible with Flow 3.x
 
 Installation
 ------------
 
 Requires npm (node.js) to work out of the box, although binaries can also be installed manually without it.
 
-```composer require "moc/imageoptimizer" "1.0.*"```
+```composer require "moc/imageoptimizer" "~2.0"```
 
 Run `npm install --prefix Resources/Private/Library` in the package (should happen automatically during install).
 
@@ -25,6 +27,13 @@ Using the ``Settings`` configuration, multiple options can be adjusted.
 
 Optimization can be disabled for specific file formats.
 
-Additionally options for optimization level (png & gif), progressive (jpg), pretty (svg) can be adjusted. 
+Additionally options for optimization level (png & gif), progressive (jpg), pretty (svg) can be adjusted.
 
 Usage of global available binaries can be configured instead or for specific formats.
+
+Usage
+_____
+
+Clear thumbnails to generate new ones that will automatically be optimized.
+
+```./flow media:clearthumbnails```
